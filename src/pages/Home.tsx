e1ac785e8a7454c5e80f3705b91f9c5eaa4ae2a9
AlShaikh <abdullah@alshai5.com>
2022-03-23 20:08:57 +0300

    Styling for home page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,37 +1,50 @@
-import { IonCard, IonCardContent, IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonCard,
+  IonCardContent,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonImg,
+  IonPage,
+  IonRow,
+} from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-        <h1 className='ion-padding'>Emirates Mars Mission</h1>
+        <h1 className='ion-padding title'>Emirates Mars Mission</h1>
+        <IonImg src='/assets/mars-hope-probe.png' style={{
+          'max-width': '400px',
+          'margin': '20px auto'
+        }}></IonImg>
         <IonGrid>
           <IonRow>
             <IonCol>
-              <IonCard className='card-button' routerLink={`/videos`} button={true}>
-                <IonCardContent>
+              <IonCard className='card-button emm-videos' routerLink={`/videos`} button={true}>
+                <IonCardContent className='main-card-content'>
                   EMM Videos
                 </IonCardContent>
               </IonCard>
             </IonCol>
             <IonCol>
-              <IonCard className='card-button' routerLink={`/gravity`} button={true}>
-                <IonCardContent>
+              <IonCard className='card-button gravity' routerLink={`/gravity`} button={true}>
+                <IonCardContent className='main-card-content'>
                   Gravity
                 </IonCardContent>
               </IonCard>
             </IonCol>
             <IonCol>
-              <IonCard className='card-button' routerLink={`/mars-vs-earth`} button={true}>
-                <IonCardContent>
-                  Mars vs Earth
+              <IonCard className='card-button mars-vs-earth' routerLink={`/mars-vs-earth`} button={true}>
+                <IonCardContent className='main-card-content'>
+                  Mars <span className='tiny'>vs</span> Earth
                 </IonCardContent>
               </IonCard>
             </IonCol>
             <IonCol>
-              <IonCard className='card-button' routerLink={`/book`} button={true}>
-                <IonCardContent>
+              <IonCard className='card-button emm-book' routerLink={`/book`} button={true}>
+                <IonCardContent className='main-card-content'>
                   EMM Book
                 </IonCardContent>
               </IonCard>
