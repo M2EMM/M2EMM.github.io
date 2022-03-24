aee232c981139a854a4598723b2cf59b834fab90
AlShaikh <abdullah@alshai5.com>
2022-03-24 22:25:48 +0300

    Updated gravity pages.

--- src/pages/Gravity.tsx
+++ src/pages/Gravity.tsx
@@ -1,19 +1,28 @@
 import {
+  InputChangeEventDetail,
+  InputCustomEvent,
   IonBackButton,
   IonButtons,
-  IonCard,
-  IonCardContent,
   IonContent,
   IonInput,
-  IonLabel,
   IonPage,
 } from '@ionic/react';
 import { useState } from 'react';
 import './Gravity.css';
 
 const Gravity: React.FC = () => {
-  const [earth, setEarth] = useState<string>();
-  const [mars, setMars] = useState<string>();
+  const [earth, setEarth] = useState<number>();
+  const [mars, setMars] = useState<number>();
+  const marsWeight = (event: CustomEvent<InputChangeEventDetail>) => {
+    const value = parseFloat(event.detail.value!);
+    setEarth(value);
+    setMars(parseFloat((value * (3.711 / 9.81)).toFixed(2)));
+  }
+  const earthWeight = (event: CustomEvent<InputChangeEventDetail>) => {
+    const value = parseFloat(event.detail.value!);
+    setMars(value);
+    setEarth(parseFloat((value * (9.81 / 3.711)).toFixed(2)));
+  }
   return (
     <IonPage>
       <IonButtons slot='start'>
@@ -21,18 +30,16 @@ const Gravity: React.FC = () => {
       </IonButtons>
       <IonContent fullscreen>
         <h1 className='ion-padding title title-gravity'>Gravity Game</h1>
-        <IonCard>
-          <IonCardContent>
-            <IonLabel position="stacked">My weight on Earth</IonLabel>
-            <IonInput value={earth}> </IonInput>
-          </IonCardContent>
-        </IonCard>
-        <IonCard>
-          <IonCardContent>
-            <IonLabel position="stacked">My weight on Mars</IonLabel>
-            <IonInput value={mars}> </IonInput>
-          </IonCardContent>
-        </IonCard>
+        <h2 className='ion-text-center title subtitle-gravity'>My weight on Earth</h2>
+        <div className='input-field earth'>
+          <IonInput value={earth} placeholder='--' type='number' onIonChange={marsWeight}></IonInput>
+          <span>KG</span>
+        </div>
+        <h2 className='ion-text-center title subtitle-gravity'>My weight on Mars</h2>
+        <div className='input-field mars'>
+          <IonInput value={mars} placeholder='--' type='number' onIonChange={earthWeight}></IonInput>
+          <span>KG</span>
+        </div>
       </IonContent>
     </IonPage>
   );
